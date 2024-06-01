00d7d0168dbcccfed8276789bed93355f5b05669
libiunc <libriunc@gmail.com>
2024-06-02 06:15:48 +0800

    changed shells

--- build.sh
+++ build.sh
@@ -1,4 +1,31 @@
-meson setup bulid/ --prefix=/usr -Ddri-drivers-path=/usr/lib/riscv64-linux-gnu/pvr-dri \
+# meson setup bulid/ --prefix=/usr -Ddri-drivers-path=/usr/lib/riscv64-linux-gnu/pvr-dri \
+#         -Dglvnd=true -Dglvnd-vendor-name=pvr \
+#         -Dgallium-drivers=pvr -Dvulkan-drivers=pvr \
+#         -Dglx=disabled -Dllvm=disabled -Dgbm=enabled
+
+# ninja -C "$SRCDIR"/abbuild
+
+# abinfo "Installing ..."
+# mkdir -pv "$PKGDIR"/usr/lib "$PKGDIR"/usr/share/glvnd/egl_vendor.d
+# DESTDIR="$SRCDIR"/tmp ninja -C "$SRCDIR"/abbuild install
+# cp -av "$SRCDIR"/tmp/usr/lib/*pvr* "$PKGDIR"/usr/lib/
+# cp -rv "$SRCDIR"/tmp/usr/lib/gbm "$PKGDIR"/usr/lib/
+# ln -sfv pvr_gbm.so "$PKGDIR"/usr/lib/gbm/starfive_gbm.so
+# cp -v "$SRCDIR"/tmp/usr/share/glvnd/egl_vendor.d/50_pvr.json "$PKGDIR"/usr/share/glvnd/egl_vendor.d/40_pvr.json
+
+
+
+meson setup build/ --prefix=/usr -Ddri-drivers-path=/usr/lib/riscv64-linux-gnu/pvr-dri \
         -Dglvnd=true -Dglvnd-vendor-name=pvr \
         -Dgallium-drivers=pvr -Dvulkan-drivers=pvr \
         -Dglx=disabled -Dllvm=disabled -Dgbm=enabled
+
+ninja -C build
+
+echo "Installing ..."
+mkdir -pv /usr/lib /usr/share/glvnd/egl_vendor.d
+ninja -C build install
+cp -av /tmp/usr/lib/*pvr* /usr/lib/
+cp -rv /tmp/usr/lib/gbm /usr/lib/
+ln -sfv pvr_gbm.so /usr/lib/gbm/starfive_gbm.so
+cp -v /tmp/usr/share/glvnd/egl_vendor.d/50_pvr.json /usr/share/glvnd/egl_vendor.d/40_pvr.json
--- env_install.sh
+++ env_install.sh
@@ -2,4 +2,4 @@
 apt-get  install llvm-dev byacc bison meson flex python3-pkgconfig cmake valgrind libwayland-egl-backend-dev  libunwind-dev libxcb-dri2-0-dev libxcb-dri3-dev xcb-proto libxcb-util-dev libxshmfence-dev
 pip install packaging mako ninja --break-system-packages
 #python env check
-#deb requires install
+#deb requires install 可能是我太菜了，这构建环境要的东西确实有一点多
